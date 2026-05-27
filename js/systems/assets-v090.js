2ed2551a2e1bcf8ad5ed38a17333c40e820e0666
TDTRIA <skullingtonskully@gmail.com>
2026-05-26 20:09:55 -0400

    Add v0.9.0 asset manifest

---
+++ js/systems/assets-v090.js
@@ -0,0 +1,37 @@
+// LEGEND v0.9.0 - Asset Manifest
+// Centralizes asset paths so new art can be wired without hunting through systems.
+(() => {
+  window.LegendAssetsV090 = {
+    logo: {
+      main: 'assets/ui/logos/logo_legend_main_v1.png',
+      emblem: 'assets/ui/logos/logo_legend_emblem_v1.png'
+    },
+    title: {
+      background: 'assets/title/title_bg_ashmere_road_v089.jpg'
+    },
+    locations: {
+      ashmereMain: 'assets/locations/ashmere/location_ashmere_mainstreet_v1.jpg',
+      oldRoad: 'assets/locations/roads/location_old_road_main_v1.jpg'
+    },
+    npcs: {
+      mara: 'assets/npcs/ashmere/npc_mara_portrait_v1.jpg',
+      brenn: 'assets/npcs/ashmere/npc_brenn_portrait_v1.jpg',
+      oric: 'assets/npcs/ashmere/npc_oric_portrait_v1.jpg',
+      innkeeper: 'assets/npcs/vendors/npc_innkeeper_portrait_v1.jpg',
+      blacksmith: 'assets/npcs/vendors/npc_blacksmith_portrait_v1.jpg',
+      trader: 'assets/npcs/vendors/npc_trader_portrait_v1.jpg'
+    },
+    enemies: {
+      roadGoblin: 'assets/enemies/road/enemy_road_goblin_v1.jpg',
+      blackrootWolf: 'assets/enemies/road/enemy_blackroot_wolf_v1.jpg',
+      lanternWisp: 'assets/enemies/road/enemy_lantern_wisp_v1.jpg'
+    },
+    items: {
+      food: 'assets/items/consumables/item_food_rations_v1.png',
+      potion: 'assets/items/consumables/item_healing_potion_v1.png',
+      roadToken: 'assets/items/quest/item_road_token_v1.png',
+      bellFragment: 'assets/items/quest/item_bell_fragment_v1.png',
+      pelt: 'assets/items/materials/item_pelt_v1.png'
+    }
+  };
+})();
